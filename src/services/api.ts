b378a97cce544c04fc3fc986baa91980eea42419
renanrzo <arenan1988@gmail.com>
2023-05-28 14:20:15 -0300

    fix api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: 'https://json-server-h9f5.onrender.com',
+    baseURL: apiUrl,
 });
 
 export default api;
