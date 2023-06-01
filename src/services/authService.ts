b49c8ac307281b1a67b547021a535c4754c111d8
renanrzo <arenan1988@gmail.com>
2023-06-01 20:04:46 -0300

    Revert "update 31-05-2023"
    This reverts commit 22a28d66f79f7e8838f4ea17b837e90689a30cb5.

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,11 +5,6 @@ export interface User {
     password: string;
 }
 
-<<<<<<< HEAD
-export const login = async (User: User): Promise<User> => {
-    const response = await api.post<User>("/auth/login", User);
-    return response.data;
-=======
 export const login = async (loginData: LoginData): Promise<User> => {
     const user = await getUserByEmail(loginData.email);
 
@@ -18,5 +13,4 @@ export const login = async (loginData: LoginData): Promise<User> => {
     } else {
         throw new Error ("Email e/ou password inválido(s).");
     }
->>>>>>> parent of 22a28d6 (update 31-05-2023)
 }
