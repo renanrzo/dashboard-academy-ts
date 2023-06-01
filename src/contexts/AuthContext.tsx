ecb55f2875b3030c2a5a107a88c0ab72cc9849be
renanrzo <arenan1988@gmail.com>
2023-05-31 21:29:51 -0300

    update two 31-05-2023

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 
-import { User } from "../services/UserService";
+import { User } from "../services/authService";
 
 interface AuthContextProps {
     authenticated: boolean;
