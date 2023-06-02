90083159cc86995f0d59901163bf2f93228f1655
renanrzo <arenan1988@gmail.com>
2023-06-01 21:29:50 -0300

    fix 01-06-2023

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,16 +1,11 @@
-import { User, getUserByEmail } from "./UserService";
+import api from './api';
 
 export interface User {
     email: string;
     password: string;
 }
 
-export const login = async (loginData: LoginData): Promise<User> => {
-    const user = await getUserByEmail(loginData.email);
-
-    if (user && user.password === loginData.password) {
-        return user;
-    } else {
-        throw new Error ("Email e/ou password inválido(s).");
-    }
+export const login = async (User: User): Promise<User> => {
+    const response = await api.post<User>("/auth/login", User);
+    return response.data;
 }
