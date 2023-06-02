2831ff0dc13f9745c95873071678ec4224e47f31
renanrzo <arenan1988@gmail.com>
2023-06-02 20:30:12 -0300

    fix baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL;
+// const apiUrl = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: apiUrl,
+    baseURL: "https://site-pessoal-api-fizg.onrender.com/api",
 });
 
 export default api;
